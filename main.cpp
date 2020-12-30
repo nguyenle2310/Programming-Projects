ff7705b2b070b3f5e3298e128198a18de1f012ca
nguyenle2310 <42166152+nguyenle2310@users.noreply.github.com>
2020-12-30 15:45:32 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,75 +1,70 @@
+#include <iostream> // import libraries
+using namespace std; // header and body
 
-#include <iostream>
-
-using namespace std;
+// declare the identifiers
 
 double apples;
 double banana;
 double watermeton;
 double blueberry;
 double wallet = 100;
-double userchoice;
+int userchoice;
+string userName;
 
-void Hello();
+void hello()
+    {
+        cout << "What is your name?"<< endl;
+        getline(cin, userName);
+        cout << "HELLO " << userName << "!\n\n";
+    }
 
 int main()
 {
-    Hello();
-
-    void Hello()
-    {
-        string userName;
-        cout << "What is your name?"<< endl;
-        getline(cin, userName);
-        cout << "Hello " << userName << "!" << endl;
-    }
+    hello();     //call out the hello function
 
     while(wallet >= 0)
     {
-        cout << "\n\n Welcome to Grocery list \n\n";
+        cout << "\n\n Welcome to the Grocery Menu \n\n";
         cout << "Menu \n\n";
         cout << "1  Apple      $2 \n";
         cout << "2  Banana     $1 \n";
-        cout << "3  Watermelon $3 " << endl;
-        cout << "4  Blueberry  $4 \n\n";
-        cout << "You have\n";
-        cout << apples << "\n apples \n";
-        cout << banana << "\n bananas\n";
-        cout << watermeton << "\n watermelon\n";
-        cout << blueberry << "\n blueberry \n";
+        cout << "3  Watermelon $3 \n";
+        cout << "4  Blueberry  $4 \n";
+        cout << "5  Exit          \n";
+        cout << "\n You have \n";
+        cout << apples << " apples" << endl;
+        cout << banana << " bananas" << endl;
+        cout << watermeton << " watermelon" << endl;
+        cout << blueberry << " blueberry" << endl;
         cout << "Your remaining balance is $" << wallet <<  ".\n\n";
-        cout << "Type the number of fruit choice or type 0 to check out \n\n";
+        cout << "Enter your choice: \n\n";
         cin >> userchoice;
 
-        if (userchoice ==1)
+        switch(userchoice)
         {
+        case 1: 
             apples++;
             wallet = wallet - 2;
-        }
-        if (userchoice ==2)
-        {
+            break;
+        case 2:
             banana++;
             wallet = wallet - 1;
-        }
-        if (userchoice ==3)
-        {
+            break;
+        case 3:
             watermeton++;
             wallet = wallet - 3;
-        }
-        if (userchoice ==4)
-        {
+            break;
+        case 4:
             blueberry++;
             wallet = wallet - 4;
-        }
-        if (userchoice ==0)
-        {
-            cout << "\n\n You just check out. Thank you for shopping with us! \n\n";
+            break;
+        case 5:
+            cout << "\nYou have just check out. Thank you for shopping with us! \n\n";
             cout << "Your remaining balance is $" << wallet <<  ".\n\n";
             cin.get();
             return 0;
         }
     }
-
     cin.get();
     return 0;
 }
